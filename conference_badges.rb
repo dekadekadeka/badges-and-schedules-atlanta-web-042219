1f5af2298cbda3dfc1b744a0add9d5995c8be7a5
Renee Deka Ambia <dekadekadeka@gmail.com>
2019-04-02 15:48:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,12 @@
 # Write your code here.
 def badge_maker(name)
+  puts ("Hello, my name is #{name}.")
 end
 
-def batch_badge_creator(name)
+def batch_badge_creator(names)
+  for name in names
+    puts "Hello, my name is #{name}."
+  end
 end
 
 def assign_rooms(name, room)
